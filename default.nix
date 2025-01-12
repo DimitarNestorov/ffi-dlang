2a702d9dcbb2a0104501e1ed4fac8cbf0fb32694
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2025-01-12 15:42:44 +0200

    Using Zig instead of Clang

--- default.nix
+++ default.nix
@@ -1,26 +1,27 @@
 {
 	pkgs ? import <nixpkgs> { },
 	lib ? pkgs.lib,
+	stdenvNoCC ? pkgs.stdenvNoCC,
 	llvmPackages ? pkgs.llvmPackages_18,
+	target ? stdenvNoCC.hostPlatform.config,
 }:
 
-let
-	stdenv = llvmPackages.stdenv;
-in stdenv.mkDerivation (finalAttrs: {
+stdenvNoCC.mkDerivation (finalAttrs: {
 	name = "test-ffi-clang-dlang-ldc-llvm";
 
 	src = ./src;
 
 	buildInputs = [
-		llvmPackages.llvm
+		(pkgs.zig.override { inherit llvmPackages; })
 		(pkgs.ldc.override { llvm_18 = llvmPackages.llvm; })
 	];
 
 	buildPhase = ''
+		export ZIG_GLOBAL_CACHE_DIR=$(mktemp -d)
 		ldc2 -c calc.d
-		clang -c main.c
+		zig cc -c main.c
 		mkdir -p $out/bin
-		clang main.o calc.o -o $out/bin/program
+		zig build-exe calc.o main.o -lc -femit-bin=$out/bin/program
 	'';
 
 	meta = {
